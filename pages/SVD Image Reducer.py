a5867bf58dd12c15705ceb758eb6309ff2ed6b27
farazyounus11 <40037013+farazyounus11@users.noreply.github.com>
2024-04-24 15:49:43 -0400

    SVD Image Reducer.py

--- pages/SVD Image Reducer.py
+++ pages/SVD Image Reducer.py
@@ -2,8 +2,9 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+import io
 
-st.set_option('deprecation.showPyplotGlobalUse', False)  # Disable the warning
+st.set_option('deprecation.showPyplotGlobalUse', False)
 st.markdown("## Upload a Random Photo to Reduce Photo Size Using SVD")
 st.markdown("## Apps Such as Instagram/WhatsApp/Zoom reduce Image/Video Size to save user data and data center computation")
 
@@ -13,27 +14,25 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Original Image', use_column_width=True)
 
-    # Resize the image to reduce computational load
-    image_resized = image.resize((200, 200))  # Adjust the size as needed
-    A = np.array(image_resized)  # Convert PIL image to NumPy array
-    X = np.mean(A, -1)  # Convert RGB to grayscale
+    image_resized = image.resize((200, 200))
+    A = np.array(image_resized)
+    X = np.mean(A, -1)
 
-    # Perform Singular Value Decomposition (SVD)
     U, S, VT = np.linalg.svd(X, full_matrices=False)
     S = np.diag(S)
 
-    # Create a single Matplotlib figure
     fig, ax = plt.subplots(2, 2, figsize=(10, 10))
 
     for i, r in enumerate([5, 20, 100, 200]):
-        # Construct approximate image
         Xapprox = U[:, :r] @ S[0:r, :r] @ VT[:r, :]
-
-        # Display the image in the corresponding subplot
         ax[i // 2, i % 2].imshow(256 - Xapprox, cmap='gray')
         ax[i // 2, i % 2].axis('off')
         ax[i // 2, i % 2].set_title('r = ' + str(r))
 
-    # Adjust layout and display the Matplotlib figure
+        buffer = io.BytesIO()
+        plt.imsave(buffer, 256 - Xapprox, cmap='gray', format='png')
+        buffer.seek(0)
+        st.download_button(label=f"Download r={r}", data=buffer, file_name=f"svd_image_r_{r}.png")
+
     plt.tight_layout()
     st.pyplot(fig)
