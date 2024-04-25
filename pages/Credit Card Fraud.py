61c30dd9203852c106cacb2a15344c2fd2e863c5
farazyounus11 <40037013+farazyounus11@users.noreply.github.com>
2024-04-25 03:36:26 -0400

    Credit Card Fraud.py

--- pages/Credit Card Fraud.py
+++ pages/Credit Card Fraud.py
@@ -53,6 +53,7 @@ def train_and_plot_confusion_matrix(X, y):
     ax.set_title('Confusion Matrix')
     st.pyplot(fig)
 
-st.sidebar.title("Run or Rerun the Model")
-if st.sidebar.button("Run"):
+
+st.title("Run or Rerun the Model")
+if st.button("Run"):
     train_and_plot_confusion_matrix(X, y)
