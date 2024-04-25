900d0271b886b9b9b3afc62526e4378db6cedbf8
farazyounus11 <40037013+farazyounus11@users.noreply.github.com>
2024-04-25 02:21:27 -0400

    Credit Card Fraud.py

--- pages/Credit Card Fraud.py
+++ pages/Credit Card Fraud.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report, confusion_matrix
-import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
