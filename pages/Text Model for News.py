5309a4ae495e5562c2bb6de16251a9f3416d47b9
farazyounus11 <40037013+farazyounus11@users.noreply.github.com>
2024-04-25 03:23:07 -0400

    Text Model for News.py

--- pages/Text Model for News.py
+++ pages/Text Model for News.py
@@ -58,7 +58,7 @@ def load_models():
 
 st.title('News Classification App')
 st.markdown("### This Model predicts news category such as Business, Sports, World, Tech/Science")
-st.markdown("### Input a text to see what the model predicts")
+st.markdown("### Paste a random news article to see predicted label")
 
 # Text input
 text_input = st.text_area('Enter text here:', '')
