5ff713ae583e88e2edfa8b066b90bf57b0109075
farazyounus11 <40037013+farazyounus11@users.noreply.github.com>
2024-06-15 03:59:09 -0400

    I_Model.py

--- pages/I_Model.py
+++ pages/I_Model.py
@@ -74,9 +74,7 @@ if uploaded_file is not None:
         figure, axes = plt.subplots(2, 6, figsize=(27, 12))
         axes = axes.flatten()
         cm = plt.cm.RdBu
-        colors = ["red","blue" , "green" ]
-
-        cm_bright = ListedColormap(colors)
+        cm_bright = ListedColormap(["#FF0000", "#0000FF"])
 
         # Split the dataset into training and testing parts
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
