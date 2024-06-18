f2a43349d462448a5e085ec21f13587fb8814178
farazyounus11 <40037013+farazyounus11@users.noreply.github.com>
2024-06-18 15:45:21 -0400

    Home.py

--- Home.py
+++ Home.py
@@ -12,3 +12,26 @@ st.title("I am a Masters Data Science Student!")
 st.write("## The side bar has some of the apps I created!")
 st.image("stats.png", caption='Different Statistical Models doing Classification!')
 st.sidebar.success("Select a page above.")
+
+def get_pdf_files(directory):
+    pdf_files = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".pdf"):
+            pdf_files.append(filename)
+    return pdf_files
+
+# Main Streamlit app code
+def main():
+    st.title("Faraz Younus Data Science Projects in PDF Format")
+    current_directory = os.getcwd()
+    pdf_files = get_pdf_files(current_directory)
+
+    selected_file = st.selectbox("Click the Dropdown Menu to Select a Project!", pdf_files)
+
+    # Display the selected PDF file
+    if selected_file:
+        pdf_path = os.path.join(current_directory, selected_file)
+        pdf_viewer(pdf_path)
+
+if __name__ == "__main__":
+    main()
