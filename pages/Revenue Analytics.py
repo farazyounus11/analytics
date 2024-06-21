63f7480b32e740ff058de940b63116b0482b0b11
farazyounus11 <40037013+farazyounus11@users.noreply.github.com>
2024-06-21 16:17:14 -0400

    Revenue Analytics.py

--- pages/Revenue Analytics.py
+++ pages/Revenue Analytics.py
@@ -165,6 +165,24 @@ comdf['Most common pair'] = comdf['Most common pair'].apply(lambda x: f"{x[0]} &
 
 
 
+avgysales = res.groupby(by="YEAR")['SALES'].sum().reset_index()
+avgmsales = res.groupby(by="MONTH")['SALES'].sum().reset_index()
+
+# Streamlit app
+st.title("Sales Analysis")
+
+# Create two columns
+col1, col2 = st.columns(2)
+
+# Column 1: Average Yearly Sales
+with col1:
+    st.header("Average Yearly Sales")
+    st.line_chart(data=avgysales, x='YEAR', y='SALES')
+
+# Column 2: Average Monthly Sales
+with col2:
+    st.header("Average Monthly Sales")
+    st.line_chart(data=avgmsales, x='MONTH', y='SALES')
 
 
 
