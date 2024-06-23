1f9aa8100453cd92ccb9a6605a007399b427e86c
farazyounus11 <40037013+farazyounus11@users.noreply.github.com>
2024-06-23 19:22:00 -0400

    I_Model.py

--- pages/I_Model.py
+++ pages/I_Model.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import os
+import random
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
