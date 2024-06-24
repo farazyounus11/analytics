f278cfffed66a12106636b948e5330d9ceb60a11
farazyounus11 <40037013+farazyounus11@users.noreply.github.com>
2024-06-24 18:06:53 -0400

    Update Stock_Filter.py

--- pages/Stock_Filter.py
+++ pages/Stock_Filter.py
@@ -50,7 +50,7 @@ create_data = {
                 "Recommendation_Key": "multiselect"}
 
 
-all_widgets = sp.create_widgets(df, create_data, ignore_columns=["Name", "Long_Business_Summary","Shareholder_Rights_Risk","Compensation_Risk", "Open"])
+all_widgets = sp.create_widgets(df, create_data, ignore_columns=["Name", "Long_Business_Summary","Shareholder_Rights_Risk","Compensation_Risk", "Open","Fifty_Two_Week_Low", "Fifty_Two_Week_High"])
 res = sp.filter_df(df, all_widgets)
 
 
