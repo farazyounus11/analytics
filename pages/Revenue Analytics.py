d35c07c1c920066c99225ce53a868955e7600f76
farazyounus11 <40037013+farazyounus11@users.noreply.github.com>
2024-06-24 18:20:49 -0400

    Update Revenue Analytics.py

--- pages/Revenue Analytics.py
+++ pages/Revenue Analytics.py
@@ -171,7 +171,15 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.header("Average Yearly Sales")
-    st.line_chart(data=avgysales)
+    plt.figure(figsize=(10, 6))
+    plt.bar(avgysales.index.astype(str), avgysales.values, color='blue')
+    plt.title('Average Yearly Sales')
+    plt.xlabel('Year')
+    plt.ylabel('Total Sales')
+    plt.xticks(rotation=45)
+    plt.gca().yaxis.set_major_formatter(FuncFormatter(lambda x, _: '{:,.0f}'.format(x)))
+    st.pyplot(plt)
+
 with col2:
     st.header("Total Monthly Sales - Line Chart")
 # Plotting with Matplotlib
