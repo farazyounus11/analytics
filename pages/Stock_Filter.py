e756d06664a749564f71e7b6a390d718972a684f
farazyounus11 <40037013+farazyounus11@users.noreply.github.com>
2024-06-24 22:19:57 -0400

    Update Stock_Filter.py

--- pages/Stock_Filter.py
+++ pages/Stock_Filter.py
@@ -72,20 +72,20 @@ sorted_by_total_cash = res.sort_values(by='Total_Cash', ascending=False).head(15
 sorted_by_forward_eps = res.sort_values(by='Forward_EPS', ascending=False).head(15)[['Name', 'Forward_EPS']].round({'Forward_EPS': 1})
 sorted_by_net_income = res.sort_values(by='Net_Income_To_Common', ascending=False).head(15)[['Name', 'Net_Income_To_Common']].round({'Net_Income_To_Common': 1})
 
-# Display in Streamlit using HTML
+# Display in Streamlit
 col1, col2, col3 = st.columns(3)
 
 with col1:
     st.markdown('### Sorted by Total Cash')
-    st.write(sorted_by_total_cash.to_html(index=False, justify='left'), unsafe_allow_html=True)
+    st.dataframe(sorted_by_total_cash)  # Don't display index column
 
 with col2:
     st.markdown('### Sorted by Forward EPS')
-    st.write(sorted_by_forward_eps.to_html(index=False, justify='left'), unsafe_allow_html=True)
+    st.dataframe(sorted_by_forward_eps)  # Don't display index column
 
 with col3:
     st.markdown('### Sorted by Net Income to Common')
-    st.write(sorted_by_net_income.to_html(index=False, justify='left'), unsafe_allow_html=True)
+    st.dataframe(sorted_by_net_income)  # Don't display index column
 
 
 
