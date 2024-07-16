e6cd01990287dbd2828409f287d8a9d1f0e90b43
farazyounus11 <40037013+farazyounus11@users.noreply.github.com>
2024-07-15 22:12:51 -0400

    Update Stocks PCA.py

--- pages/Stocks PCA.py
+++ pages/Stocks PCA.py
@@ -13,10 +13,8 @@ import os
 
 target_directory = '/mount/src/analytics/pages/'
 os.chdir(target_directory)
-st.write("Current Directory:", os.getcwd())
 
-current_directory = os.getcwd()
-st.write("Current Directory:", current_directory)
+
 
 
 @st.cache_data
