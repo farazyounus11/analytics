cf59c40ee531effd556453642d0023e7cbef56f8
farazyounus11 <40037013+farazyounus11@users.noreply.github.com>
2024-09-08 01:29:35 -0400

    Update Ecommerce.py

--- pages/Ecommerce.py
+++ pages/Ecommerce.py
@@ -6,9 +6,7 @@ import pydeck as pdk
 @st.cache_data
 def load_comdf3():
     df = pd.read_csv("ecomm1.csv")
-    df['Transaction Date'] = pd.to_datetime(df['Transaction Date'])
-    df.to_csv("ecomm2.csv", index=False)
-    
+    df['Transaction Date'] = pd.to_datetime(df['Transaction Date'])    
     return df
     
 comdf = load_comdf3()
@@ -29,27 +27,27 @@ with col2:
         default=['Completed'])
 
 if selected_categories:
-    filtered_df = comdf[comdf['Product Category'].isin(selected_categories)]
+    com_filtered_df = comdf[comdf['Product Category'].isin(selected_categories)]
 
 if purchase_status:
-    filtered_df = filtered_df[filtered_df['Purchase Completed'].isin(purchase_status)]
+    com_filtered_df = com_filtered_df[com_filtered_df['Purchase Completed'].isin(purchase_status)]
 
-if not filtered_df.empty:
-    st.metric(label="Number of Transactions", value=len(filtered_df))
-    transaction_counts_by_date = filtered_df.groupby(['Transaction Date', 'Product Category']).size().unstack(fill_value=0)
+if not com_filtered_df.empty:
+    st.metric(label="Number of Transactions", value=len(com_filtered_df))
+    transaction_counts_by_date = com_filtered_df.groupby(['Transaction Date', 'Product Category']).size().unstack(fill_value=0)
     st.header('Map', divider='gray')
     st.pydeck_chart(pdk.Deck(
         map_style=None,
         initial_view_state=pdk.ViewState(
-            latitude=filtered_df['Latitude'].mean(),
-            longitude=filtered_df['Longitude'].mean(),
+            latitude=com_filtered_df['Latitude'].mean(),
+            longitude=com_filtered_df['Longitude'].mean(),
             zoom=11,
             pitch=50,
         ),
         layers=[
             pdk.Layer(
                 'HexagonLayer',
-                data=filtered_df,
+                data=com_filtered_df,
                 get_position='[Longitude, Latitude]',
                 radius=100,
                 elevation_scale=4,
@@ -59,7 +57,7 @@ if not filtered_df.empty:
             ),
             pdk.Layer(
                 'ScatterplotLayer',
-                data=filtered_df,
+                data=com_filtered_df,
                 get_position='[Longitude, Latitude]',
                 get_color=[200, 30, 0, 160],
                 get_radius=100,
