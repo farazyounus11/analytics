6c6514e18d7364ec7da1c2c7430c7143a61fe1f2
farazyounus11 <40037013+farazyounus11@users.noreply.github.com>
2024-09-08 01:10:50 -0400

    Update Ecommerce.py

--- pages/Ecommerce.py
+++ pages/Ecommerce.py
@@ -2,49 +2,16 @@ import streamlit as st
 import pandas as pd
 import pydeck as pdk
 
-st.set_page_config(layout="wide")
+comdf = pd.read_csv("ecomm1.csv")
+comdf['Transaction Date'] = pd.to_datetime(comdf['Transaction Date'])
 
-# Load the ecomm CSV file
-@st.cache_data
-def load_dataframe(file_path):
-    df = pd.read_csv(file_path)
-    df['Transaction Date'] = pd.to_datetime(df['Transaction Date'])
-    return df
+product_categories = comdf['Product Category'].unique()
+selected_categories = st.multiselect("Select Product Categories", options=product_categories, default=['Automotive Parts'])
+purchase_status = st.multiselect("Purchase Status", options=comdf['Purchase Completed'].unique(), default=['Completed'])
 
-# Path to your CSV file
-file_path = "ecomm1.csv"  # Replace with your actual path if different
-df = load_dataframe(file_path)
-
-st.markdown("## E-commerce Transactions Visualization")
-st.markdown("### Use the Sidebar to Filter Data")
-
-try:
-    min_date = df['Transaction Date'].min().to_pydatetime()
-    max_date = df['Transaction Date'].max().to_pydatetime()
-    selected_start_date, selected_end_date = st.sidebar.slider(
-        "Select date range",
-        min_value=min_date,
-        max_value=max_date,
-        value=(min_date, max_date)
-    )
-except Exception as e:
-    st.error(f"Error with date slider: {str(e)}")
-
-# Filter by Product Category
-product_categories = df['Product Category'].unique()
-selected_categories = st.sidebar.multiselect("Select Product Categories", options=product_categories, default=['Automotive Parts'])
-
-# Filter by Purchase Completed
-purchase_status = st.sidebar.multiselect("Purchase Status", options=df['Purchase Completed'].unique(), default=['Completed'])
-
-# Apply filters
-filtered_df = df[
-    (df['Transaction Date'] >= selected_start_date) & 
-    (df['Transaction Date'] <= selected_end_date)
-]
 
 if selected_categories:
-    filtered_df = filtered_df[filtered_df['Product Category'].isin(selected_categories)]
+    filtered_df = comdf[comdf['Product Category'].isin(selected_categories)]
 
 if purchase_status:
     filtered_df = filtered_df[filtered_df['Purchase Completed'].isin(purchase_status)]
